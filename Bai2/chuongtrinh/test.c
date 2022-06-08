2ffa105b7eaeb7c682bd291dbf297ed21bda376a
Mitchell Vu <mitchell.vu0208@gmail.com>
2022-06-08 16:25:14 +0700

    feat: course template

---
+++ Bai2/chuongtrinh/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "abc.h"
+#define $ dola
+int $;
+
+int main()
+{
+    abc();
+    $ = 100;    
+    printf("%d", $);
+
+    return 0;
+}
+
---
+++ Bai4/day3/incompleted/semantics.c
@@ -0,0 +1,50 @@
+/* 
+ * @copyright (c) 2008, Hedspi, Hanoi University of Technology
+ * @author Huu-Duc Nguyen
+ * @version 1.0
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "semantics.h"
+#include "error.h"
+
+extern SymTab* symtab;
+extern Token* currentToken;
+
+Object* lookupObject(char *name) {
+  // TODO
+}
+
+void checkFreshIdent(char *name) {
+  // TODO
+}
+
+Object* checkDeclaredIdent(char* name) {
+  // TODO
+}
+
+Object* checkDeclaredConstant(char* name) {
+  // TODO
+}
+
+Object* checkDeclaredType(char* name) {
+  // TODO
+}
+
+Object* checkDeclaredVariable(char* name) {
+  // TODO
+}
+
+Object* checkDeclaredFunction(char* name) {
+  // TODO
+}
+
+Object* checkDeclaredProcedure(char* name) {
+  // TODO
+}
+
+Object* checkDeclaredLValueIdent(char* name) {
+  // TODO
+}
+
